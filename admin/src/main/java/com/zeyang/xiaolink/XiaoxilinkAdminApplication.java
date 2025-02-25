4444081d32470821dfd696bc18ebbd714df64eed
Chaoshi Wu <2843287911@qq.com>
2025-02-26 00:32:25 +0800

    添加SpringBoot等依赖
    添加spring-boot依赖，以及在父类pom.xml文件中添加lombok公共依赖；
    在父模块中添加开发作者信息。

--- admin/src/main/java/com/zeyang/xiaolink/XiaoxilinkAdminApplication.java
+++ admin/src/main/java/com/zeyang/xiaolink/XiaoxilinkAdminApplication.java
@@ -1,5 +1,11 @@
 package com.zeyang.xiaolink;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class XiaoxilinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoxilinkAdminApplication.class, args);
+    }
 }
