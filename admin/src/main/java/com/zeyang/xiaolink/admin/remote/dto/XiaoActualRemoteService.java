ba43fe0873e92688b7ccd2e008d47fcb93111354
Chaoshi Wu <2843287911@qq.com>
2025-02-27 23:42:12 +0800

    用户登录模块开发

---
+++ admin/src/main/java/com/zeyang/xiaolink/admin/remote/dto/XiaoActualRemoteService.java
@@ -0,0 +1,14 @@
+package com.zeyang.xiaolink.admin.remote.dto;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * zeyang
+ */
+//@FeignClient(
+//        value = "xiaoxilink-admin",
+//        url = "${aggregation.remote-url:}",
+//        configuration = OpenFeignConfiguration.class
+//)
+public interface XiaoActualRemoteService {
+}
